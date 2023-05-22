e04aa82ec7df58fb1a43ef625c045ef02e3d04fd
Random amber <randomamber@Data-iMac.local>
2023-05-22 10:50:31 +0300

    Update working code

--- tech_tutorial/network.py
+++ tech_tutorial/network.py
@@ -1,33 +1,31 @@
 import socket
+import pickle
+
 
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #self.server = "192.168.49.74" surffi
         self.server = "10.10.10.111"
         self.port = 5555
         self.addr = (self.server, self.port)
-        self.pos = self.connect()
-        #print(self.id)
+        self.p = self.connect()
 
-    def getPos(self):
-        return self.pos
-    
+    def getP(self):
+        return self.p
+
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return self.client.recv(2048).decode()
+            return pickle.loads(self.client.recv(2048))
         except:
             pass
 
     def send(self, data):
         try:
-            self.client.send(str.encode(data))
-            return self.client.recv(2048).decode()
+            self.client.send(pickle.dumps(data))
+            return pickle.loads(self.client.recv(2048))
         except socket.error as e:
             print(e)
 
-  #test          
-# n = Network()
-# print(n.send("hello"))
-# print(n.send("working"))    
+
+
